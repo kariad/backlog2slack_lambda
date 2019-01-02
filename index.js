ab4dc65c8ac6c955a14662b1457fe5e14708adc5
Daiki Katayama <karuiad.venezia@gmail.com>
2019-01-02 21:47:09 +0900

    Add minimum function.

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
+var Slack = require("slack-node");
 var BOTNAME = process.env['BOTNAME'];
 var BASEURL = process.env['BASEURL'];
 var WEBHOOKURI = process.env['WEBHOOKURI'];
+var CHANNELNAME = process.env['CHANNELNAME'];
+var COLOR = process.env['COLOR'];
+var BacklogType = {
+    1: "課題の追加",
+    2: "課題の更新",
+    3: "課題にコメント"
+};
+function createDescription(event) {
+    switch (event.type) {
+        case 1:
+        case 2:
+            return event.content.description;
+        case 3:
+            return event.content.comment.content;
+        default:
+            return event.content.description;
+    }
+}
 function handler(event, context, callback) {
-    console.log("Hello Wold!!!!");
+    var slack = new Slack;
+    slack.setWebhook(String(WEBHOOKURI));
+    var url = BASEURL + "-" + event.content.key_id;
+    var attachment = {
+        "color": COLOR,
+        "pretext": "Backlogに更新がありました。",
+        "title": event.content.summary,
+        "title_link": url,
+        "fields": [
+            {
+                "title": "更新種別",
+                "value": BacklogType[+event.type],
+                "short": true
+            },
+            {
+                "title": "更新者",
+                "value": event.createdUser.name,
+                "short": true
+            },
+            {
+                "title": "内容",
+                "value": createDescription(event),
+                "short": false
+            }
+        ]
+    };
+    slack.webhook({
+        channel: CHANNELNAME,
+        username: String(BOTNAME),
+        attachments: [attachment]
+    }, function (err, response) {
+        console.log(err);
+    });
 }
 exports.handler = handler;
--- index.ts
+++ index.ts
@@ -1,10 +1,69 @@
-import * as slackNode from 'slack-node'
-import { APIGatewayEvent, Context, Callback, APIGatewayEventRequestContext } from 'aws-lambda';
+import Slack = require("slack-node");
+import { Context, Callback } from 'aws-lambda';
 
-var BOTNAME = process.env['BOTNAME'];
-var BASEURL = process.env['BASEURL'];
-var WEBHOOKURI = process.env['WEBHOOKURI'];
+const BOTNAME = process.env['BOTNAME'];
+const BASEURL = process.env['BASEURL'];
+const WEBHOOKURI = process.env['WEBHOOKURI'];
+const CHANNELNAME = process.env['CHANNELNAME'];
+const COLOR = process.env['COLOR'];
 
-export function handler(event: APIGatewayEvent, context: Context, callback: Callback) {
-    console.log("Hello Wold!!!!");
+interface BacklogEvent {
+    created: String;
+    type: Number
+}
+
+const BacklogType: { [key: number]: string} = {
+    1: "課題の追加",
+    2: "課題の更新",
+    3: "課題にコメント"
+}
+
+function createDescription(event: any): string {
+    switch (event.type) {
+        case 1:
+        case 2:
+            return event.content.description
+        case 3:
+            return event.content.comment.content
+        default:
+            return event.content.description
+    }
+}
+
+export function handler(event: any, context: Context, callback: Callback) {
+    let slack = new Slack;
+    slack.setWebhook(String(WEBHOOKURI));
+    let url = BASEURL + "-" + event.content.key_id
+
+    let attachment = {
+        "color": COLOR,
+        "pretext": "Backlogに更新がありました。",
+        "title": event.content.summary,
+        "title_link": url,
+        "fields": [
+            {
+                "title": "更新種別",
+                "value": BacklogType[+event.type],
+                "short": true
+            },
+            {
+                "title": "更新者",
+                "value": event.createdUser.name,
+                "short": true
+            },
+            {
+                "title": "内容",
+                "value": createDescription(event),
+                "short": false
+            }
+        ]
+    };
+
+    slack.webhook({
+        channel: CHANNELNAME,
+        username: String(BOTNAME),
+        attachments: [attachment]
+      }, function(err, response) {
+        console.log(err);
+      });
 }
